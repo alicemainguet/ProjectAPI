6689498d20559db7bedcf0b1d868dc00f1db98d2
alicemainguet <50203696+alicemainguet@users.noreply.github.com>
2021-07-06 10:08:05 +0300

    search queries

--- api/routes/sites.js
+++ api/routes/sites.js
@@ -39,7 +39,84 @@ router.get('/', async (req, res) => {
 */ 
 
 
+
+//delete a site 
+router.delete('/:id', async (req, res) => {
+  try {
+    const site = await Site.findById(req.params.id)
+    if (site == null) {
+      return res.status(404).json("Cannot find site")
+    }
+    await site.remove()
+    res.json("site deleted successfully")
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+
+
+
+
+//--------------------------------SEARCH OPERATIONS
+
 //get all sites with possible selection
+
+router.get("/", async (req, res) => {
+  const typeofsite = req.query.typeis;
+  const searchField = req.query.keyword;
+  try {
+    let sites;
+     if (typeofsite) {
+      sites = await Site.find({
+        siteType: {
+          $in: [typeofsite],
+        },
+      });
+     } else if (searchField) {
+      sites = await Site.find({$or: [
+        { description: { $regex: searchField,$options: '$i'}},
+        { siteName: { $regex: searchField,$options: '$i'}} 
+      ]})
+     } else {
+      sites = await Site.find();
+    }
+      res.status(200).json(sites);
+  } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+//get all sites with possible selection WORKS
+/*
+router.get("/", async (req, res) => {
+  const typeofsite = req.query.typeis;
+  const searchField = req.query.siteName;
+  try {
+    let sites;
+     if (typeofsite) {
+      sites = await Site.find({
+        siteType: {
+          $in: [typeofsite],
+        },
+      });
+     } else if (searchField) {
+      sites = await Site.find({
+        siteName: {
+          $regex: searchField,$options: '$i'
+        },
+      });
+     } else {
+      sites = await Site.find();
+    }
+      res.status(200).json(sites);
+  } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+*/
+/*
+//search by category
 router.get("/", async (req, res) => {
   const typeofsite = req.query.typeis;
   try {
@@ -60,22 +137,24 @@ router.get("/", async (req, res) => {
   });
 
 
-
-//delete a site 
-router.delete('/:id', async (req, res) => {
-  try {
-    const site = await Site.findById(req.params.id)
-    if (site == null) {
-      return res.status(404).json("Cannot find site")
-    }
-    await site.remove()
-    res.json("site deleted successfully")
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
-
-
-
-
+//search by keyword
+  router.get("/", async (req, res) => {
+    const keyword = req.query.keyword;
+    try {
+      let sites;
+       if (keyword) {
+        sites = await Site.find({
+          $or: [
+            { description: { $regex: keyword,$options: '$i'}},
+            { siteName: { $regex: keyword,$options: '$i'}} 
+          ]})
+       } else {
+        sites = await Site.find();
+      }
+        res.status(200).json(sites);
+    } catch (err) {
+        res.status(500).json(err);
+      }
+    });
+*/
 module.exports = router
